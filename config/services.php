249d94ea8358fd15b5ba3fa16f31c6c7dcfb7a33
sergemezui <sergemezui.dev@gmail.com>
2025-12-07 18:49:41 +0100

    feat(project:setup): configure message handler tagging
    Added `_instanceof` configuration for automatic tagging of command, query, and event handlers in Symfony Messenger. This streamlines message bus setup and improves maintainability of the application's messaging infrastructure.

--- config/services.php
+++ config/services.php
@@ -11,5 +11,19 @@ return App::config([
         'CleanArchi\\' => [
             'resource' => '../src/',
         ],
+        '_instanceof' => [
+            'CleanArchi\\Shared\\Application\\Messaging\\CommandHandler' => [
+                'tags' => ['name' => 'messenger.message_handler', 'bus' => 'command.bus'],
+            ],
+            'CleanArchi\\Shared\\Application\\Messaging\\QueryHandler' => [
+                'tags' => ['name' => 'messenger.message_handler', 'bus' => 'query.bus'],
+            ],
+            'CleanArchi\\Shared\\Application\\Messaging\\MessageHandler' => [
+                'tags' => ['name' => 'messenger.message_handler', 'bus' => 'message.bus'],
+            ],
+            'CleanArchi\\Shared\\Application\\EventListener\\EventListener' => [
+                'tags' => ['name' => 'messenger.message_handler'],
+            ],
+        ],
     ],
 ]);
