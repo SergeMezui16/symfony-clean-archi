0da22cbfc88222c34cd3338acdcbb049e83e3a6f
sergemezui <sergemezui.dev@gmail.com>
2025-12-07 16:59:27 +0100

    feat(project:setup): integrate Twig templating engine
    Added Twig dependencies and configured `twig.yaml` for template handling. Created a base Twig template and updated the project namespace to align with the new structure.

--- src/Shared/Infrastructure/Framework/Symfony/Kernel.php
+++ src/Shared/Infrastructure/Framework/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace CleanArchi\Shared\Infrastructure\Framework\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
